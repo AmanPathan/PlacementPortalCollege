2ed2c06060c46a3e966797019ab7d4f902508961
AmanPathan <amanpathan526@gmail.com>
2024-03-05 23:56:52 +0530

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -9,52 +9,60 @@ import Company from './components/Company';
 import Internship from './components/Internship';
 import Hackathon from './components/Hackathon';
 import Profile from './components/Profile';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Analytics } from "@vercel/analytics/react";
 import Demo from './components/Demo';
 
-function App() {
-  const URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=tXsxy2-hvwS31b9L92CSnDeGZ3CQNEO1_cFYVGNdKtG1HykzKeta_lLa9mHb-85_JxKtFebIpJoA88k0vdcRRVxmI6kAp73Cm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDogGNI0BST8X8uIhrZnF83Ik5WQDsaGUaum2emq-uGmqccBjBlNb7s21kAJBYLaTnc6Qy88gk9KnUhosPf3J4uecRGUEjL7GQ&lib=Mu7cgZbbJR3Jsd9m20phPH86idZU4jP8o';
-  const URL1 = "https://script.google.com/macros/s/AKfycbyQQE80wVyNKq8OMRigxzicAAVHrTUsCF0jXt4NOoPItsCmR9V9KPF5M0v_mxa1qQzd/exec";
-  
-  const [studentsData, setData] = useState([]);
-  const [internshipData, setInternshipData] = useState([]);
-  
-  const fetchData = async () => {
-    const res = await axios.get(URL);
-    localStorage.setItem('data', JSON.stringify(res.data.data));
-  }
+import { get, ref } from 'firebase/database';
+import { useState, useEffect } from 'react';
+import { database } from '../src/firebaseConfig.js';
 
+function App() {
+  // const URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=tXsxy2-hvwS31b9L92CSnDeGZ3CQNEO1_cFYVGNdKtG1HykzKeta_lLa9mHb-85_JxKtFebIpJoA88k0vdcRRVxmI6kAp73Cm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDogGNI0BST8X8uIhrZnF83Ik5WQDsaGUaum2emq-uGmqccBjBlNb7s21kAJBYLaTnc6Qy88gk9KnUhosPf3J4uecRGUEjL7GQ&lib=Mu7cgZbbJR3Jsd9m20phPH86idZU4jP8o';
+  const URL1 = "https://script.google.com/macros/s/AKfycbyQQE80wVyNKq8OMRigxzicAAVHrTUsCF0jXt4NOoPItsCmR9V9KPF5M0v_mxa1qQzd/exec";
+
+  const [studentsData, setStudents] = useState([]);
+  const [internshipData, setInternshipData] = useState([]);
+
+  //internship
   const getData = async () => {
     const response = await axios.get(URL1);
     setInternshipData(response.data.data);
   };
 
+
+
   useEffect(() => {
     getData();
-    
-    fetchData();
-    const data_items = JSON.parse(localStorage.getItem('data'));
-    if (data_items) {
-      setData(data_items);
-    }
-
-  }, [])
+    const studentsRef = ref(database, 'Students');
+    get(studentsRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const studentsArray = Object.entries(snapshot.val()).map(([id, data]) => ({
+          id, ...data,
+        }));
+        setStudents(studentsArray);
+      }
+      else {
+        console.log("No Data Available!");
+      }
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, []);
 
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Demo/>} />
-        <Route path='/dypcoe' element={<Forms/>} />
-        <Route path='/dashboard' element={<Dashboard data={studentsData}/>} />
-        <Route path='/students' element={<Student data={studentsData}/>} />
-        <Route path='/companies' element={<Company data={studentsData}/>} />
-        <Route path='/hackathons' element={<Hackathon/>} />
-        <Route path='/internships' element={<Internship internshipData={internshipData}/>} />
-        <Route path='/students/:ID' element={<Profile data={studentsData}/>} />
-        <Route path='*' element={<Home/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/dypcoe' element={<Forms />} />
+        <Route path='/dashboard' element={<Dashboard data={studentsData} />} />
+        <Route path='/students' element={<Student data={studentsData} />} />
+        <Route path='/companies' element={<Company data={studentsData} />} />
+        <Route path='/hackathons' element={<Hackathon />} />
+        <Route path='/internships' element={<Internship internshipData={internshipData} />} />
+        <Route path='/students/:ID' element={<Profile data={studentsData} />} />
+        <Route path='*' element={<Home />} />
       </Routes>
       <Analytics />
     </>
